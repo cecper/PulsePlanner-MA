26666ea77af09255b948872ca62ee453ba2e17eb
KevinVerbruggen1 <78963576+KevinVerbruggen1@users.noreply.github.com>
2023-11-05 17:11:17 +0100

    add training pagina

--- app/src/main/java/com/example/pulseplanner/MainActivity.kt
+++ app/src/main/java/com/example/pulseplanner/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_exercise, R.id.nav_category, R.id.nav_exercise_overview
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_exercise, R.id.nav_category, R.id.nav_exercise_overview, R.id.nav_add_training
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
